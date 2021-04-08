32d6a526ea56769ca282d97c6bc437fcd0bbb03c
muhanz <muhanz@umich.edu>
2021-04-08 20:34:22 +0800

    Add ListenManager

--- crawler/include/CrawlerManager.h
+++ crawler/include/CrawlerManager.h
@@ -7,7 +7,26 @@
 // Task Input: URLs from crawler / URLs from other machines
 // Task: Distribute URLs according to hash and obey bloom filter
 // Task Output: URLs to frontier / URLs to other machines
+size_t numMachine = 7;
+int port = 0;
+size_t queue_size = 1024;
+int make_server_sockaddr(struct sockaddr_in *addr, int port) {
+	// Step (1): specify socket family.
+	// This is an internet socket.
+	addr->sin_family = AF_INET;
 
+	// Step (2): specify socket address (hostname).
+	// The socket will be a server, so it will only be listening.
+	// Let the OS map it to the correct address.
+	addr->sin_addr.s_addr = INADDR_ANY;
+
+	// Step (3): Set the port value.
+	// If port is 0, the OS will choose the port for us.
+	// Use htons to convert from local byte order to network byte order.
+	addr->sin_port = htons(port);
+
+	return 0;
+}
 class CrawlerManager : public ThreadPool
     {
     public:
@@ -18,6 +37,7 @@ class CrawlerManager : public ThreadPool
     private:   
         Frontier *frontier;
         FileBloomfilter *visited;
+        size_t myIndex;
     };
 
 class ListenManager : public CrawlerManager 
--- crawler/src/CrawlerManager.cpp
+++ crawler/src/CrawlerManager.cpp
@@ -1,14 +1,71 @@
 #include "CrawlerManager.h"
-
+#include "Common.h"
+#define MAX_MESSAGE_SIZE 512
 void ListenManager::DoTask( Task task, size_t threadID )
     {
     while ( alive )
         {
         // Listen on a socket for incoming links
+        // Create a TCP/IP socket.
+        int socketFD = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+        // (2) Set the "reuse port" socket option
+	    int yesval = 1;
+	    if (setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR, &yesval, sizeof(yesval)) == -1) {
+            perror("Error setting socket options");
+            return -1;
+	    }
+        // (3) Create a sockaddr_in struct for the proper port and bind() to it.
+        struct sockaddr_in addr;
+        if (make_server_sockaddr(&addr, port) == -1) {
+            return -1;
+        }
+        // (3b) Bind to the port.
+        if (bind(socketFD, (sockaddr *) &addr, sizeof(addr)) == -1) {
+            perror("Error binding stream socket");
+            return -1;
+        }
 
-        // Verify URL hash
+        // (3c) Detect which port was chosen.
+	    port = get_port_number(socketFD);
 
-        // Push the URL to the frontier
+        // (4) Begin listening for incoming connections.
+	    listen(socketFD, queue_size);
+
+        while ( true )
+            {
+            int connectionfd = accept(socketFD, 0, 0);
+		    assert (connectionfd > -1);
+            // (1) Receive message from client.
+
+            char msg[MAX_MESSAGE_SIZE + 1];
+            memset(msg, 0, sizeof(msg));
+
+            // Call recv() enough times to consume all the data the client sends.
+            size_t recvd = 0;
+            ssize_t rval;
+            do {
+                // Receive as many additional bytes as we can in one call to recv()
+                // (while not exceeding MAX_MESSAGE_SIZE bytes in total).
+                rval = recv(connectionfd, msg + recvd, MAX_MESSAGE_SIZE - recvd, 0);
+                assert ( rval > -1 );
+                recvd += rval;
+            } while (rval > 0);  // recv() returns 0 when client closes
+
+            // (2) Print out the message
+            printf("Client %d says '%s'\n", connectionfd, msg);
+
+            // (4) Close connection
+            close(connectionfd);
+
+            // Verify URL hash
+            uint32_t hash = fnvHash((const char *)msg, recvd);
+            if ( hash % numMachine == this->myIndex ) 
+                {
+                // Push the URL to the frontier
+                this->frontier->PushUrl(Link(msg));
+                }
+            }
+        
         }
     }
 
