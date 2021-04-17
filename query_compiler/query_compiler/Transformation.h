ec088c6b66b4e1b88342e77caeef80c066d0e0cc
hongxy1999 <78655607+hongxy1999@users.noreply.github.com>
2021-04-17 07:36:10 -0400

    Undo: Normalization of the query

---
+++ query_compiler/query_compiler/Transformation.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <../utility/include/mString.h>
+#include <../index/include/ISR.h>
+#include <../constraint_solver/AbstractISR.h>
+#include <fstream>
+
+ISR* StringToISR ( Dictionary *dict, char* input ) ;
