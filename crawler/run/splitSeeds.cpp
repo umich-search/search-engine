77adc5b6f62c59ac1bc77df11992756f4ba46ed0
ziyuan-h <ziyuanh@umich.edu>
2021-04-17 14:03:08 +0800

    14-seedlists

--- crawler/run/splitSeeds.cpp
+++ crawler/run/splitSeeds.cpp
@@ -6,23 +6,30 @@
 using namespace std;
 
 const char *seedlist = "top-1m.txt";
-const char *seedMs[ 7 ] = 
+const char *seedMs[ 14 ] = 
     {
-    "seedlist/seedM1.txt",
-    "seedlist/seedM2.txt",
-    "seedlist/seedM3.txt",
-    "seedlist/seedM4.txt",
-    "seedlist/seedM5.txt",
-    "seedlist/seedM6.txt",
-    "seedlist/seedM7.txt",
+    "seedlist/seedM0-0.txt",
+    "seedlist/seedM0-1.txt",
+    "seedlist/seedM1-0.txt",
+    "seedlist/seedM1-1.txt",
+    "seedlist/seedM2-0.txt",
+    "seedlist/seedM2-1.txt",
+    "seedlist/seedM3-0.txt",
+    "seedlist/seedM3-1.txt",
+    "seedlist/seedM4-0.txt",
+    "seedlist/seedM4-1.txt",
+    "seedlist/seedM5-0.txt",
+    "seedlist/seedM5-1.txt",
+    "seedlist/seedM6-0.txt",
+    "seedlist/seedM6-1.txt",
     };
 
 int main( )
     {
     ifstream in;
-    ofstream out[ 7 ];
+    ofstream out[ 14 ];
     in.open( seedlist );
-    for ( int i = 0; i < 7; ++i )
+    for ( int i = 0; i < 14; ++i )
         out[ i ].open( seedMs[ i ] );
     string s;
     while ( in >> s )
@@ -32,7 +39,7 @@ int main( )
         out[ idx ] << s << '\r' 
             << '\n';
         }
-    for ( int i = 0; i < 7; ++i )
+    for ( int i = 0; i < 14; ++i )
         out[ i ].close( );
     in.close( );
     }
