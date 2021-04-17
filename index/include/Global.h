3a716e494b54ab72fc4ec69e6af97e5dc1979592
Andrew Jiang <andrewjiang@Andrews-MacBook-Pro.local>
2021-04-17 02:14:23 -0400

    Update parameters to optimize for 200mb chunk sizes

--- index/include/Global.h
+++ index/include/Global.h
@@ -10,9 +10,9 @@
 #include "Common.h"
 
 // Max number of sync points
-#define NUM_SYNC_POINTS 64 // 8
+#define NUM_SYNC_POINTS 512 // 8
 // Size of bytes to write in memory before writing to disk
-#define CHUNK_SIZE_BYTES 524288000 // 1000
+#define CHUNK_SIZE_BYTES 209715200 // 1000
 // titles > MAX_TITLE_LENGTH will be cutoff
 #define MAX_TITLE_LENGTH 512
 // Documents with URLs > MAX_URL_LENGTH will be skipped
@@ -23,9 +23,9 @@
 #define MAX_PATHNAME_LENGTH 4096
 // Storage locations
 #define CHUNKS_METADATA_PATH "index_files/chunks_metadata.txt"
-#define CHUNKS_METADATA_DIRECTORY "index_files/"
-#define CHUNK_DIRECTORY "index_files/"
-#define DOCS_DIRECTORY "index_files/"
+#define CHUNKS_METADATA_DIRECTORY "/Users/andrewjiang/Desktop/eecs440/search-engine/crawler/run/index_files/"
+#define CHUNK_DIRECTORY "/Users/andrewjiang/Desktop/eecs440/search-engine/crawler/run/index_files/"
+#define DOCS_DIRECTORY "/Users/andrewjiang/Desktop/eecs440/search-engine/crawler/run/index_files/"
 // Whether or not to write to disk (set to false for in-memory tests)
 //#define WRITE_TO_DISK true
 // Whether or not to write every chunk bytes ( used for testing)
