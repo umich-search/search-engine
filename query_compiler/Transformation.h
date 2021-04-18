6838aed0506e6106a8846df57642ef1cbf09dbf3
hongxy1999 <78655607+hongxy1999@users.noreply.github.com>
2021-04-18 01:19:15 -0400

    Delete query_compiler directory

--- query_compiler/Transformation.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <../utility/include/mString.h>
-#include <../index/include/ISR.h>
-#include <../constraint_solver/AbstractISR.h>
-#include <fstream>
-
-ISR* StringToISR ( Dictionary *dict, char* input ) ;
