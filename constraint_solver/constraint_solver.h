3197f3cc52f504c14bde118a50c10d783c255007
zhongzh <zhongzh@umich.edu>
2021-04-17 23:32:12 -0400

    constraint solver output vector of Match* of tuple (id, start, end)

--- constraint_solver/constraint_solver.h
+++ constraint_solver/constraint_solver.h
@@ -2,4 +2,11 @@
 #include "../utility/include/Common.h"
 #include "../utility/include/Vector.h"
 
-::vector<Offset>* ConstraintSolver(ISREndDoc* EndDoc, ISR* queryRoot);
+struct Match
+   {
+   Offset id;
+   Location start, end;
+   Match(Offset d, Location s, Location e): id(d), start(s), end(e){}
+   };
+
+::vector<Match*>* ConstraintSolver(ISREndDoc* EndDoc, ISR* queryRoot);
