40aef50673e2ebbdc2e65061dc2097e363e901d6
Jiaxin Ying <jiaxinyi@umich.edu>
2021-04-19 00:39:46 -0400

    start ranker

--- ranker/ISRSpan.h
+++ ranker/ISRSpan.h
@@ -7,11 +7,22 @@
 
 class ISRSpan{
 public:
-    void Next();
+    //Move the rarest term to the next occurrence
+    //Move all the other terms to the nearest location corresponding to the rarest term;
+    //Return True if the end of the doc is reached
+    bool Next();
 
-    void Start(Location startLocation);
+    //Move the rarest term to the first occurrence in the document specified by docIndex
+    //Move all other terms to the nearest location corresponding to the rarest term;
+    //Update the docIndex to be equal to index
+    void Start(size_t index);
 
+    //Update the score corresponding to the doc specified by docIndex
+    //The location of all terms in current span is given by location
     void update_score();
+
+    //Return the calculated score
+    ::vector<float> get_score();
 
 private:
     //the total number of terms in the phrase
@@ -23,13 +34,12 @@ private:
     //the ISRWord abstract for other terms
     ISRWord** Terms;
 
-    Location startDocLocation;
-
-    Location endDocLocation;
+    //the current index of document in ConstraintSolver
+    size_t docIndex = 0;
 
     ::vector<Match*>* ConstraintSolver;
 
     ::vector<Location> location;
 
-    float score;
+    ::vector<float> score;
 };
