e2fb2d42b7a2e875be985d359c727bc1dc159eb8
zhongzh <zhongzh@umich.edu>
2021-04-25 01:02:51 -0400

    another .h......

--- constraint_solver/include/constraint_solver.h
+++ constraint_solver/include/constraint_solver.h
@@ -4,18 +4,17 @@
 #include "Common.h"
 #include "Vector.h"
 
-
-::vector<Match*>* ConstraintSolver(ISREndDoc* EndDoc, ISR* queryRoot);
-
 class ConstraintSolver
    {
    public:
-      ConstraintSolver();
+      ConstraintSolver(ISREndDoc* EndDoc, ISR* queryRoot);
       // return the first match starting from location currentLocation
-      Match* findMatch(ISREndDoc* EndDoc, ISR* queryRoot);
+      Match* findMatch();
       // manually set currentLocation to begin search
       void setLocation(Location location);
 
    private:
       Location currentLocation;
+      ISREndDoc* EndDoc;
+      ISR* queryRoot;
    }
